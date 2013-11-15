11e35ad4897a3e6b44370ec5face7ed96f1d8c01
proliberate <andrew@proliberate.org>
2013-11-15 16:14:19 -0500

    help with has_many through

--- db/migrate/20131115173029_create_appointments.rb
+++ db/migrate/20131115173029_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.belongs_to :student_id
-      t.belongs_to :classrecord_id
+      t.references :student_id
+      t.references :classrecord_id
 
       t.timestamps
     end
